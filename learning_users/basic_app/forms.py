c77c938caf8a684fba032e136d135cdb8a19fd1f
Mohd Al Khasawneh <43467769+MEKH1@users.noreply.github.com>
2022-04-13 15:04:53 +0300

    first

---
+++ learning_users/basic_app/forms.py
@@ -0,0 +1,16 @@
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import User
+from basic_app.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password') 
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfo
+        fields = ('profolio_site','profile_pic')
